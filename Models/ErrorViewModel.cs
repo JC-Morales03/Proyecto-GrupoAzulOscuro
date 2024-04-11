b5327dce65a21ae746732fa0f11595bee35c2a07
JC-Morales03 <juan_morales8@usmp.pe>
2024-04-11 13:51:29 -0500

    Commit Inicial

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace Proy_GrupoAzulOscuro.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
